ff4ef2ea8250404af2d3d4f79330b15e0923e360
sinagun <38187655+sinagun@users.noreply.github.com>
2021-08-05 19:57:32 +0000

    0.4

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
+
+
 import discord
 import os
+import requests
+import json
+
+TOKEN= 'token here'
 
 client = discord.Client()
 
+def get_quote():
+  resource=requests.get("https://zenquotes.io/api/random")
+
+  json_data = json.loads(resource.text)
+
+  quote= json_data[0]['q']+" -" + json_data[0]['a']
+  return(quote)
+
+
+
+
 @client.event
 async def on_ready():
-  print('we have logged in as{0.user}'.format(client))
+  print('we have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
   if message.author == client.user:
     return
 
-  if message.content.startswith('$hello'):  
-    await message.channel.send('hello!')
+  if message.content.startswith('$inspire'):
+    quote = get_quote()
+    await message.channel.send(quote)
 
-client.run(os.get1.env('TOKEN'))
+client.run(TOKEN)
