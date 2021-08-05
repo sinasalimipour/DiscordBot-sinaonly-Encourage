9939c7bad16ac387cff7e3c1817fd0a6b7309699
sinagun <38187655+sinagun@users.noreply.github.com>
2021-08-05 16:05:11 +0000

    0.2

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import discord
+
+client = discord.client()
+@client.event
+async def on_ready():
+  print("we have logged in as{0.user}".format(client))
+
+  @client.event
+  async def on_message(message):
+    if message.author == client.user:
+      return
+    if message.content.startswith('$hello'):  
+      await message.channel.send('hello!')
+
+  client.run()
